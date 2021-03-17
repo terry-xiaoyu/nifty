2021d745de32231036bcca064032ff8ba0e7de4a
Shawn <506895667@qq.com>
2021-03-17 19:32:21 +0800

    Fix build issues on Mac OSX

--- travis/fix_exports.sh
+++ travis/fix_exports.sh
@@ -3,7 +3,7 @@ then
     echo "ec_semver_parser.erl ... ok"
 else
     echo "Supress export_all warning in ec_semver_parser.erl"
-    sed -i "s/-compile(export_all)\./-compile(export_all)\.\n-compile(nowarn_export_all)\./g" _build/default/lib/erlware_commons/src/ec_semver_parser.erl
+    sed -i'' -e "s/-compile(export_all)\./-compile(export_all)\.\n-compile(nowarn_export_all)\./g" _build/default/lib/erlware_commons/src/ec_semver_parser.erl
 fi
 
 if grep -q "nowarn_export_all" _build/default/lib/relx/src/rlx_goal.erl
@@ -11,5 +11,5 @@ then
     echo "rlx_goal.erl         ... ok"
 else
     echo "Supress export_all warning in rlx_goal.erl"
-    sed -i "s/-compile(export_all)\./-compile(export_all)\.\n-compile(nowarn_export_all)\./g" _build/default/lib/relx/src/rlx_goal.erl
+    sed -i'' -e "s/-compile(export_all)\./-compile(export_all)\.\n-compile(nowarn_export_all)\./g" _build/default/lib/relx/src/rlx_goal.erl
 fi
